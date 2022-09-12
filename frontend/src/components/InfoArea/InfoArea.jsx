989a7747e6dce39ea1b2c0ff83593c6c42f5cbe3
CaioNathanS <nathansallenave@gmail.com>
2022-09-11 23:59:08 -0300

    correções css

--- frontend/src/components/InfoArea/InfoArea.jsx
+++ frontend/src/components/InfoArea/InfoArea.jsx
@@ -6,7 +6,7 @@ export function InfoArea() {
     <div>
         <div className='infos'> 
             <div className='info-item atendimento'>
-                 <h1> Atendimento </h1>
+                 <p className='info-title'> Atendimento </p>
                  <p className='warning'> Fique por dentro de nossos horários <br/> de atendimento para sua conveniência.</p>
                  <b> Segunda - Sexta </b>
                  <p> 7h - 19h</p>
@@ -14,7 +14,7 @@ export function InfoArea() {
                  <p> 7h - 12h30</p>
             </div>
             <div className='info-item local'>
-                <h1> Localização </h1>
+                <p className='info-title'> Localização </p>
                 <p className='warning'>  <i class="fa fa-home" aria-hidden="true"></i> 
                  {''} Rua Paraná, S/N  <br/>
                    Quadra 9, Lote 11 e 12 <br/>
@@ -23,14 +23,14 @@ export function InfoArea() {
                 </p>
             </div>
             <div className='info-item contato'>
-                 <h1> Contato </h1>
+                 <p className='info-title'> Contato </p>
                     <i className='fa fa-phone first'/> <a href='#'> {' +55 (64)3822-0000'}</a> <br/>
                     <i className='fa fa-whatsapp'/> <a href='#'> {' +55 (64)93822-0000'}</a>  <br/>
                     <i className='fa fa-envelope-o'/> <a href='#'> {' clinica@email.com'}</a> <br/>
                    
             </div>
             <div className='info-item social'>
-                 <h1>Redes Sociais </h1>
+                 <p className='info-title'>Redes Sociais </p>
                  <i className='fa fa fa-instagram'/> <a href='#'> {' clinica'}</a> 
             </div>
         </div>
