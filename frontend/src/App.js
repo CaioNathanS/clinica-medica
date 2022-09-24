b42319ab4ce0e8fc6827dd568b7efb795dd93a0c
CaioNathanS <nathansallenave@gmail.com>
2022-09-24 16:54:34 -0300

    modificações

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,8 +14,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/new" element={<HomeScreen />} />
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/old" element={<LandingPage />} />
         <Route path="/consultas" element={<ConsultsPage />} />
         <Route path="/exames" element={<ExamsPage />} />
         <Route path="/sobre" element={<AboutPage />} />
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -20,7 +20,7 @@ export function Header({change}) {
     },
     {
      label:'Contato',
-     value:'edit',
+     value:bg,
     },
   ]
 
